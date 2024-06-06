c65b9d5fbf45f818a1c4330d9a9bd10b27fbcc6b
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-06-06 13:34:12 +1000

    tests for different keyOnWindow and prefix

--- loggingHelper.test.js
+++ loggingHelper.test.js
@@ -2,7 +2,7 @@ import LoggingHelper from './loggingHelper';
 
 const windowRef = {}
 
-test('creates properly with defaults', () => {
+test('It creates properly with defaults', () => {
   const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
 
   expect(windowRef.__LOGGING.namespace).toMatchObject({
@@ -13,3 +13,25 @@ test('creates properly with defaults', () => {
     ERROR: false,
   })
 })
+
+
+test('It handles a different keyOnWindow', () => {
+  const keyOnWindow = 'DIFFERENT'
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef, keyOnWindow })
+
+  expect(windowRef[keyOnWindow].namespace).toMatchObject({
+    TRACE: false,
+    DEBUG: false,
+    INFO: false,
+    WARN: false,
+    ERROR: false,
+  })
+})
+
+test('It handles outputs the correct prefix', () => {
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
+
+  expect(lh.prefix()).toBe('namespace: ')
+})
+
+
