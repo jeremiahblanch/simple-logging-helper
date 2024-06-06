d3095a4987e893b8ce683a66337cee9d33ec8a2b
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-06-06 13:43:01 +1000

    tests for shouldLogTrace, shouldLogInfo, shouldLogWarn,. shouldLogError

--- loggingHelper.test.js
+++ loggingHelper.test.js
@@ -55,3 +55,35 @@ test('shouldLogDebug handles toggling of DEBUG', () => {
   windowRef.__LOGGING.namespace.DEBUG = true;
   expect(lh.shouldLogDebug()).toBeTruthy();
 })
+
+test('shouldLogTrace handles toggling of TRACE', () => {
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
+
+  expect(lh.shouldLogTrace()).toBeFalsy();
+  windowRef.__LOGGING.namespace.TRACE = true;
+  expect(lh.shouldLogTrace()).toBeTruthy();
+})
+
+test('shouldLogInfo handles toggling of INFO', () => {
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
+
+  expect(lh.shouldLogInfo()).toBeFalsy();
+  windowRef.__LOGGING.namespace.INFO = true;
+  expect(lh.shouldLogInfo()).toBeTruthy();
+})
+
+test('shouldLogWarn handles toggling of WARN', () => {
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
+
+  expect(lh.shouldLogWarn()).toBeFalsy();
+  windowRef.__LOGGING.namespace.WARN = true;
+  expect(lh.shouldLogWarn()).toBeTruthy();
+})
+
+test('shouldLogError handles toggling of ERROR', () => {
+  const lh = new LoggingHelper({ namespace: 'namespace', windowRef})
+
+  expect(lh.shouldLogError()).toBeFalsy();
+  windowRef.__LOGGING.namespace.ERROR = true;
+  expect(lh.shouldLogError()).toBeTruthy();
+})
